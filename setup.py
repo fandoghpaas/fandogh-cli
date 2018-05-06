18545ceb67498030c0ed3def4b6cc4d9fef17ee0
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-06 10:37:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setup(
     name='fandogh_cli',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    version='0.1.0',
+    version='0.1.1',
     description='Official CLI for Fandogh PaaS',
     author='Soroosh Sarabadani',
     install_requires=["click",
