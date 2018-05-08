5ef159813ff5382164cee213b133ae6c0a084509
soroosh sarabadani <soroosh.sarabadani@klarna.com>
2018-05-08 18:08:40 +0200

    change zip implementation and exclude workspace.zip file explicitly

--- fandogh_cli/workspace.py
+++ fandogh_cli/workspace.py
@@ -1,12 +1,15 @@
 import os
-from shutil import make_archive
+import zipfile
 
 
 def build_workspace(workspace_config):
     workspace_path = workspace_config.get('path', os.getcwd())
-    zip_file_name = os.path.join(workspace_path, 'workspace')
-    make_archive(zip_file_name, 'zip')
-    return zip_file_name + '.zip'
+    zip_file_name = os.path.join(workspace_path, 'workspace.zip')
+    zipf = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
+    zipdir(workspace_path, zipf)
+    zipf.close()
+
+    return zip_file_name
 
 
 def cleanup_workspace(workspace_config):
@@ -14,3 +17,9 @@ def cleanup_workspace(workspace_config):
     zip_file_name = os.path.join(workspace_path, 'workspace.zip')
     if os.path.exists(zip_file_name):
         os.remove(zip_file_name)
+
+
+def zipdir(path, ziph):
+    for i in os.listdir(path):
+        if 'workspace.zip' not in i:
+            ziph.write(i, compress_type=zipfile.ZIP_DEFLATED)
