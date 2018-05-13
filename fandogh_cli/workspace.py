592eb8586a2d19e199780054245c3b117fa11980
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-13 17:53:57 +0200

    remove workspace.zip file after publish

--- fandogh_cli/workspace.py
+++ fandogh_cli/workspace.py
@@ -26,8 +26,8 @@ def build_workspace(workspace_config):
 def cleanup_workspace(workspace_config):
     workspace_path = workspace_config.get('path', os.getcwd())
     zip_file_name = os.path.join(workspace_path, 'workspace.zip')
-    # if os.path.exists(zip_file_name):
-    #     os.remove(zip_file_name)
+    if os.path.exists(zip_file_name):
+        os.remove(zip_file_name)
 
 
 def zipdir(path, ziph):
