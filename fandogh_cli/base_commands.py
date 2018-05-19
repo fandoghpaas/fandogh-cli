fdcb9711d27a5c5d0c0176481c1a9d900cecb17f
Mahdi Zareie <Mahdi.elf@gmail.com>
2018-05-19 14:07:24 +0430

    Rename to get_latest_version

--- fandogh_cli/base_commands.py
+++ fandogh_cli/base_commands.py
@@ -5,7 +5,7 @@ from click import Command
 from fandogh_cli import NAME
 from fandogh_cli.fandogh_client import FandoghAPIError, AuthenticationError
 from fandogh_cli.utils import debug, TextStyle, format_text
-from fandogh_cli.version_check import get_max_version, get_current_version, Version
+from fandogh_cli.version_check import get_latest_version, get_current_version, Version
 from fandogh_cli.config import get_user_config
 
 
@@ -46,12 +46,12 @@ class FandoghCommand(Command):
     def _get_max_version(self):
         cached_version_info = get_user_config().get("version_info")
         if cached_version_info is None:
-            max_version = get_max_version()
+            max_version = get_latest_version()
             last_check = datetime.now()
         else:
             last_check, max_version = cached_version_info['last_check'], Version(cached_version_info['max_version'])
             if (datetime.now() - last_check) > timedelta(hours=6):
-                max_version = get_max_version()
+                max_version = get_latest_version()
                 last_check = datetime.now()
         get_user_config().set("version_info", dict(last_check=last_check, max_version=str(max_version)),)
         return max_version
