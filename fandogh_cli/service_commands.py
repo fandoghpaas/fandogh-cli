2bace2d756e9a4c8b8a0efc4f21d7d96b706ae41
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-26 18:55:56 +0200

    set default value for port (80)

--- fandogh_cli/service_commands.py
+++ fandogh_cli/service_commands.py
@@ -28,7 +28,7 @@ def service_logs(service_name):
 @click.option('--version', '-v', prompt='The image version', help='The image version you want to deploy')
 @click.option('--name', prompt='Your service name', help='Choose a unique name for your service')
 @click.option('--env', '-e', 'envs', help='Environment variables (format: VARIABLE_NAME=VARIABLE_VALUE)', multiple=True)
-@click.option('--port', '-p', 'port', help='The service port that will be exposed on port 80 to worldwide')
+@click.option('--port', '-p', 'port', help='The service port that will be exposed on port 80 to worldwide', default=80)
 @login_required
 def deploy(image, version, name, port, envs):
     """Deploy service"""
