a2002ed695305232175b9b6f0d740818cf176467
soroosh sarabadani <soroosh.sarabadani@klarna.com>
2018-06-21 12:35:03 +0200

    rename --configuration option to --config

--- fandogh_cli/managed_service_commands.py
+++ fandogh_cli/managed_service_commands.py
@@ -2,8 +2,7 @@ import click
 
 from .fandogh_client import *
 from .utils import login_required
-from .config import get_project_config, get_user_config
-from .presenter import present
+from .config import get_user_config
 from .base_commands import FandoghCommand
 
 
@@ -16,12 +15,13 @@ def managed_service():
 @click.command("deploy", cls=FandoghCommand)
 @click.argument('name', nargs=1)
 @click.argument('version', nargs=1)
-@click.option('-c', '--configuration', 'config', help='Managed service configuration (format: VARIABLE_NAME=VARIABLE_VALUE)', multiple=True)
+@click.option('-c', '--config', 'configs', help='Managed service configuration (format: VARIABLE_NAME=VARIABLE_VALUE)',
+              multiple=True)
 @login_required
-def deploy(name, version, config):
+def deploy(name, version, configs):
     """Deploy Managed Service"""
     token = get_user_config().get('token')
-    response = deploy_managed_service(name, version, config, token)
+    response = deploy_managed_service(name, version, configs, token)
     click.echo(response.get('message'))
 
 
