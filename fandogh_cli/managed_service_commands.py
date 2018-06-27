b5626355c259f25ffe59f3fba76a26cf5befa21d
Mahdi Zareie <Mahdi.elf@gmail.com>
2018-06-27 13:40:54 +0430

    Add command for listing managed-services

--- fandogh_cli/managed_service_commands.py
+++ fandogh_cli/managed_service_commands.py
@@ -16,13 +16,23 @@ def managed_service():
 @click.command("deploy", cls=FandoghCommand)
 @click.argument('name', nargs=1)
 @click.argument('version', nargs=1)
-@click.option('-c', '--configuration', 'config', help='Managed service configuration (format: VARIABLE_NAME=VARIABLE_VALUE)', multiple=True)
+@click.option('-c', '--configuration', 'config',
+              help='Managed service configuration (format: VARIABLE_NAME=VARIABLE_VALUE)', multiple=True)
 @login_required
 def deploy(name, version, config):
     """Deploy Managed Service"""
     token = get_user_config().get('token')
     response = deploy_managed_service(name, version, config, token)
     click.echo(response.get('message'))
+
+
+@click.command("list", cls=FandoghCommand)
+@login_required
+def deploy():
+    """List available managed-services"""
+    token = get_user_config().get('token')
+    click.echo(present(lambda: get_available_managed_services(token),
+                       renderer='list'))
 
 
 managed_service.add_command(deploy)
