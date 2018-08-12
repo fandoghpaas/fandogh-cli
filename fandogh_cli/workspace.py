2fa3e3022ad3d90c7007726ec0f11b9c03bb01cd
sajad sarabadani <sajad.sarabadani@tarameshgrup.com>
2018-08-12 17:54:13 +0430

    - Extra line deleted.

--- fandogh_cli/workspace.py
+++ fandogh_cli/workspace.py
@@ -11,7 +11,6 @@ class Workspace:
         self.zip_file_name = os.path.join(self.path, 'workspace.zip')
         files = os.listdir(self.path)
         self.has_docker_ignore = '.dockerignore' in files
-        self.docker_is_prevented = ''
         self.has_docker_file = 'Dockerfile' in files
         self._create_zip_file()
         self.zip_file_size_kb = os.path.getsize(self.zip_file_name)
