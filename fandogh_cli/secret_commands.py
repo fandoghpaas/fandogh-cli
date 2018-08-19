32daabd44cb02dee852bc17a7bbc2ce6e0d67e92
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-08-19 11:18:39 +0200

    give hint if namespace does not have any secret

--- fandogh_cli/secret_commands.py
+++ fandogh_cli/secret_commands.py
@@ -1,5 +1,6 @@
 import click
 from fandogh_cli.fandogh_client.secrets_client import list_secret, create_secret
+from .utils import format_text, TextStyle
 from .presenter import present
 from .base_commands import FandoghCommand
 
@@ -16,8 +17,10 @@ def list():
                     renderer='table',
                     headers=['Name', 'Secret Type', 'Created at'],
                     columns=['name', 'type', 'created_at'])
-
-    click.echo(table)
+    if len(table.strip()):
+        click.echo(table)
+    else:
+        click.echo(format_text("There is no secret available", TextStyle.WARNING))
 
 
 @click.command("create", cls=FandoghCommand)
@@ -28,8 +31,6 @@ def create(secret_type, fields, secret_name):
     """list secrets filtered by type"""
     result = create_secret(secret_name, secret_type, fields, )
     click.echo(result['message'])
-
-
 
 
 secret.add_command(list)
