0b5e635b5921f5460efd3b617161674a895dcde9
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-08-19 11:21:20 +0200

    prompt if type is missing

--- fandogh_cli/secret_commands.py
+++ fandogh_cli/secret_commands.py
@@ -24,8 +24,8 @@ def list():
 
 
 @click.command("create", cls=FandoghCommand)
-@click.option('--name', '-n', 'secret_name', help='a unique name for secret', prompt='Name for the secret', )
-@click.option('--type', '-t', 'secret_type', help='type of secret to list', default=None)
+@click.option('--name', '-n', 'secret_name', help='a unique name for secret', prompt='Name for the secret')
+@click.option('--type', '-t', 'secret_type', help='type of secret to list', prompt="Secret Type")
 @click.option('--field', '-f', 'fields', help='fields to store in secret', multiple=True)
 def create(secret_type, fields, secret_name):
     """list secrets filtered by type"""
