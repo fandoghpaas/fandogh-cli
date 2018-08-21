fcbec67da5aac428e5c6ce47fc9c8984309a2f2b
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-08-21 11:09:36 +0200

    now to show empty lines

--- fandogh_cli/image_commands.py
+++ fandogh_cli/image_commands.py
@@ -64,7 +64,9 @@ def show_image_logs(image_name, version):
     while True:
         response = get_image_build(image_name, version, image_offset)
         image_offset = response.get('lines_count')
-        click.echo(response.get('logs'))
+        logs = response.get('logs')
+        if logs.strip():
+            click.echo(response.get('logs'))
         if response.get('state') != 'BUILDING':
             break
         sleep(1)
