bef42b2dafdfb1054b4860b5ee7b1f4fafeb8024
Mahdi Zareie <Mahdi.elf@gmail.com>
2018-08-26 13:13:48 +0430

    change command color

--- fandogh_cli/domain_commands.py
+++ fandogh_cli/domain_commands.py
@@ -82,9 +82,10 @@ def request_certificate(name):
     Request a Let's Encrypt SSL/TLS Certificate for a domain
     """
     create_certificate(name)
+    command = format_text("fandogh domain details --name {}".format(name), TextStyle.OKBLUE)
     click.echo("Your request has been submitted and we are trying to get a certificate from Let's Encrypt for your"
                "domain, it might get a few minutes to complete.\n"
-               "you can follow up using `fandogh domain details --name {}`.\n".format(name))
+               "you can follow up your request using {}".format(command))
 
 
 @click.command('details', cls=FandoghCommand)
