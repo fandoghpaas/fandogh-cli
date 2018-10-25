5f8f087c9f267fdbb3600106ed2acaf83ad8f2a2
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2018-10-26 02:32:09 +0330

    Typo edited.

--- fandogh_cli/volume_commands.py
+++ fandogh_cli/volume_commands.py
@@ -8,15 +8,15 @@ def volume():
     """Volume management commands"""
 
 
-@click.command('create', help='creates new pvc', cls=FandoghCommand)
-@click.option('--name', '-n', help='name of the volume', prompt='Volume Name')
-@click.option('--capacity', '-c', help='volume capacity', prompt='Storage Capacity')
+@click.command('create', help='Create new pvc', cls=FandoghCommand)
+@click.option('--name', '-n', help='Name of the volume', prompt='Volume Name')
+@click.option('--capacity', '-c', help='Volume capacity', prompt='Storage Capacity')
 def create_volume(name, capacity):
     click.echo(create_pvc(name, capacity))
 
 
-@click.command('delete', help='delete pvc', cls=FandoghCommand)
-@click.option('--name', '-n', help='name of the volume', prompt='Volume Name')
+@click.command('delete', help='Delete pvc', cls=FandoghCommand)
+@click.option('--name', '-n', help='Name of the volume', prompt='Volume Name')
 def delete_volume(name):
     click.echo(delete_pvc(name))
 
