d723836bef78a2e6c260caab53b95422c689e494
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2018-10-28 10:16:43 +0330

    create and delete messages added. detach option added to volume creation.

--- fandogh_cli/volume_commands.py
+++ fandogh_cli/volume_commands.py
@@ -13,13 +13,16 @@ def volume():
 @click.command('add', help='Add new volume', cls=FandoghCommand)
 @click.option('--name', '-n', help='Name of the volume', prompt='Volume Name')
 @click.option('--capacity', '-c', help='Volume capacity', prompt='Storage Capacity')
+@click.option('--detach', '-d', help='Execute request in background')
 def create_volume(name, capacity):
+    click.echo('Creating volume may take some times, please wait...')
     click.echo(create_volume_claim(name, capacity))
 
 
 @click.command('delete', help='Delete specific volume', cls=FandoghCommand)
 @click.option('--name', '-n', help='Name of the volume', prompt='Volume Name')
 def delete_volume(name):
+    click.echo('Volume delete may take some times, please wait...')
     click.echo(delete_volume_claim(name))
 
 
