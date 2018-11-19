2242054bfc7603db12751e710f0c73f49ee65ef7
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2018-11-19 11:14:05 +0330

    dict declaration changed.

--- fandogh_cli/fandogh_client/__init__.py
+++ fandogh_cli/fandogh_client/__init__.py
@@ -390,7 +390,7 @@ method list:
 
 def create_volume_claim(volume_name, capacity):
     token = get_stored_token()
-    body = dict({'name': volume_name, 'spec': {'storage': capacity}})
+    body = dict({'name': volume_name, 'storage': capacity})
     response = requests.post(base_volume_url,
                              json=body,
                              headers={'Authorization': 'JWT ' + token}
