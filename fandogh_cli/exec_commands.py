8429509c322037a97169b75d194cc0372f5ce5ab
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2019-01-13 23:34:23 +0100

    implement exec_sessions client endpoint

--- fandogh_cli/exec_commands.py
+++ fandogh_cli/exec_commands.py
@@ -1,7 +1,7 @@
 import click
 
-from .fandogh_client.socketpy import start_session
-from .fandogh_client.exec_client import post_exec
+from .fandogh_client.session_manager import start_session
+from .fandogh_client.exec_client import post_exec, post_session
 from .fandogh_client import get_details
 from .base_commands import FandoghCommand
 
@@ -13,8 +13,6 @@ from .base_commands import FandoghCommand
 @click.option('-r', '--replica')
 def exec_command(command, service, replica, interactive):
     """Exec management commands"""
-    if interactive:
-        start_session('45118d90-1770-11e9-b840-600308935232')
     if not replica:
         details = get_details(service)
         pods = details['pods']
@@ -27,5 +25,11 @@ def exec_command(command, service, replica, interactive):
                 click.echo('- {}'.format(pod_name))
 
             replica = click.prompt('Please choose one of the replicas above', type=click.Choice(pod_names))
-    response = post_exec(replica, command)
-    click.echo(response['message'])
+    if interactive:
+        click.echo('Initializing session...')
+        response = post_session(replica, command)
+        click.echo('Connection to the replica...')
+        start_session(response['session_key'])
+    else:
+        response = post_exec(replica, command)
+        click.echo(response['message'])
--- fandogh_cli/fandogh_client/exec_client.py
+++ fandogh_cli/fandogh_client/exec_client.py
@@ -4,9 +4,21 @@ from fandogh_cli.fandogh_client import base_url, get_exception
 from fandogh_cli.fandogh_client import get_stored_token
 
 base_exec_url = '%sservices/exec_commands' % base_url
+base_session_url = '%sservices/exec_sessions' % base_url
 
 
 def post_exec(pod_name, command):
+    token = get_stored_token()
+    response = requests.post(base_exec_url,
+                             headers={'Authorization': 'JWT ' + token},
+                             json={'pod_name': pod_name, 'command': command})
+    if response.status_code != 200:
+        raise get_exception(response)
+    else:
+        return response.json()
+
+
+def post_session(pod_name, command):
     token = get_stored_token()
     response = requests.post(base_exec_url,
                              headers={'Authorization': 'JWT ' + token},
