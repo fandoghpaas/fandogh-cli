eb6e29f35ada370e0489a31709fce503466c139b
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2019-01-15 21:40:31 +0100

    handle the error if service does not exist

--- fandogh_cli/exec_commands.py
+++ fandogh_cli/exec_commands.py
@@ -15,6 +15,9 @@ def exec_command(command, service, replica, interactive):
     """Exec management commands"""
     if not replica:
         details = get_details(service)
+        if not details:
+            click.echo('Service {} does not exist!'.format(service), err=True)
+            return
         pods = details['pods']
         # TODO: just find the available pods
         if len(pods) == 1:
@@ -26,10 +29,10 @@ def exec_command(command, service, replica, interactive):
 
             replica = click.prompt('Please choose one of the replicas above', type=click.Choice(pod_names))
     if interactive:
-            click.echo('Initializing session...')
-            response = post_session(replica, command)
-            click.echo('Connection to the replica...')
-            start_session(response['session_key'])
+        click.echo('Initializing session...')
+        response = post_session(replica, command)
+        click.echo('Connection to the replica...')
+        start_session(response['session_key'])
     else:
         response = post_exec(replica, command)
         click.echo(response['message'])
