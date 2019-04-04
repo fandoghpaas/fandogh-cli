9356824609bcdd20a514ea0a990fe1072dd5101f
Mahdi Zareie <mahdi.elf@gmail.com>
2019-04-04 17:40:08 +0430

    set timeout for api call

--- fandogh_cli/fandogh_client/__init__.py
+++ fandogh_cli/fandogh_client/__init__.py
@@ -431,7 +431,7 @@ def _get_manifest_document(doc_key):
 
 
 def get_fandogh_latest_version():
-    response = requests.get(base_url + "/api/latest-version")
+    response = requests.get(base_url + "/api/latest-version", timeout=5)
     if response.status_code != 200:
         return None
     else:
