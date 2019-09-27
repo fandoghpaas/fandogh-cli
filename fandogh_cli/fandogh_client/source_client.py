c5427e9cce5e84a0231c8009068ea3f7cbfff5ee
MXareie <mahdi@vivaldi.net>
2019-09-27 16:31:13 +0330

    Create client for Create and Read SourceDeployment objects

--- fandogh_cli/fandogh_client/source_client.py
+++ fandogh_cli/fandogh_client/source_client.py
@@ -27,3 +27,32 @@ def get_project_types():
     if response.status_code != 200:
         raise get_exception(response)
     return response.json()
+
+
+def create_deployment(service_name, workspace_path, manifest, monitor_callback):
+    with open(workspace_path, 'rb') as file:
+        e = encoder.MultipartEncoder(
+            fields={
+                'source': ('workspace', file, 'text/plain'),
+                'manifest': manifest
+            }
+        )
+        request_body = encoder.MultipartEncoderMonitor(e, monitor_callback)
+
+        response = get_session().post(
+            "{}/{}/deployments".format(base_sources_url, service_name),
+            data=request_body,
+            headers={'Content-Type': request_body.content_type}
+        )
+
+        if response.status_code != 200:
+            raise get_exception(response)
+        return response.json()
+
+
+def get_deployment(service_name, deployment_id):
+    response = get_session().get("{}/{}/deployments/{}".format(base_sources_url, service_name, deployment_id))
+
+    if response.status_code != 200:
+        raise get_exception(response)
+    return response.json()
